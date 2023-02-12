581d1ef0f7bd28c0b466a25a1f6a0659062510b9
probir ghosh <probirghosh@outlook.com>
2023-02-12 17:22:50 +0600

    minor issue fixed

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 
 
